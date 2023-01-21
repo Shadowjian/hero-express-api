e1f397e5b97417a3dc11df8b62648c8c30efe4b7
Shadowjian <karakalel.adorable@gmail.com>
2023-01-21 23:28:51 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const userRoute = require("./routes/user")
+require("dotenv/config")
+
+// Init Express App
+const app = express()
+
+// ENVs
+const PORT = process.env.PORT || 8000
+const DB = process.env.MONGO_URI
+
+// Middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors())
+
+// Connect to DB
+mongoose.set("strictQuery", false)
+mongoose.connect(DB).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Connected to MDB and listening on PORT ${PORT}!`)
+  )
+})
+
+// ROUTES
+
+app.use("/api/users", userRoute)
+
+module.exports = app
---
+++ routes/user.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+const User = require("../models/User")
+
+// GET ALL USERS
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+// ADD USER
+
+router.post("/join", async (req, res) => {
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password
+  })
+  try {
+    const newUser = await user.save()
+    res.status(200).json(newUser)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+module.exports = router
